afea3597cea00c9a46579fe6bde992c589ba36b9
NgcBci <bckhoadaongc26@gmail.com>
2025-10-02 22:03:05 +0700

    Add HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,12 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Khoa Dao Ngoc Bich\t24021388\tK69I-CS1\tJade2612\tbckhoadaongc26@gmail.com");
+        for (int i = 9; i >= 2;i--) {
+            System.out.println( i + " bottles of beer on the wall, " + i + " bottles of beer." );
+            System.out.println( "Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
